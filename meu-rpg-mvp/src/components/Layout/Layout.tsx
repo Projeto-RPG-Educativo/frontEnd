6ba07be93f3823ecce7add8ea6cef486cb2f84f0
paulo <paulogabrielpk0@gmail.com>
2025-08-20 17:02:44 -0300

    atualização do Layout base

--- meu-rpg-mvp/src/components/Layout/Layout.tsx
+++ meu-rpg-mvp/src/components/Layout/Layout.tsx
@@ -9,21 +9,22 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="layout-container">
       <header className="layout-header">
-        <h1>Seu Jogo Incrível!</h1>
+        <h1>colocar botoes aqui</h1>
         {/* Aqui você pode adicionar um menu, status do jogador, etc. */}
       </header>
 
       <div className="layout-content-wrapper">
         <aside className="layout-sidebar">
-          {/* Conteúdo da barra lateral. Aqui pode ser onde o HUD dos personagens e inimigos está. */}
+         
         </aside>
+        
         <main className="layout-main">
           {children}
         </main>
       </div>
 
       <footer className="layout-footer">
-        <p>© 2025 Seu Nome. Todos os direitos reservados.</p>
+        <p>© 2025 Joguinho dos Cria . Todos os direitos reservados.</p>
         {/* Outras informações como versão do jogo, créditos, etc. */}
       </footer>
     </div>
