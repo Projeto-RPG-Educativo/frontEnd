4cc20b26a631145ddc191be12a9aa90a5801d4ba
paulo <paulogabrielpk0@gmail.com>
2025-08-20 17:12:04 -0300

    Divisão do CSS por tela

---
+++ meu-rpg-mvp/src/components/pages/HUD.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// import { Player, Enemy } from '../../GameDataBank';
+import './HUD.css';
+
+interface HUDProps {
+  title: string;
+  hp: number;
+  maxHp: number;
+  isPlayer?: boolean;
+}
+
+const HUD: React.FC<HUDProps> = ({ title, hp, maxHp, isPlayer }) => {
+  return (
+    <div className="hud-card">
+      <h2>{title}</h2>
+      <progress value={hp} max={maxHp} className={isPlayer ? 'player-hp' : 'enemy-hp'}></progress>
+      <p>HP: {hp} / {maxHp}</p>
+    </div>
+  );
+};
+
+export default HUD;
